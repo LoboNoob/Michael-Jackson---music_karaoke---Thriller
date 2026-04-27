1a5ec37248170e2fe7e2628edf9a4f210cd7692f
Geovane <rochageovane05@gmail.com>
2026-04-27 19:26:29 -0300

    Musica do Michael até o primeiro refrão

--- code.py
+++ code.py
@@ -1,1 +1,105 @@
+import time
+
+texto = "..."
+texto2 = "it's close"
+texto3 = "to midnight"
+texto4 = "and something evil's lurking in the dark"
+texto5 = "..."
+texto6 = "Under the moonlight"
+texto7 = "You see a sight that almost stops your heart"
+texto8 = "..."
+texto9 = "You try to scream"
+texto10 = "but terror takes the sound before you make it"
+texto11 = ".."
+texto12 = "And you start to freeze"
+texto13 = "as horror looks you right between the eyes"
+texto14 = "."
+texto15 = "You're Paralyzed"
+texto16 = "Cause this is thriller"
+texto17 = "Thriller night"
+
+for letra in texto:
+    print(letra, end="", flush=True)
+    time.sleep(20) #20
+print()  # quebra linha
+
+for letra2 in texto2:
+    print(letra2, end="", flush=True)
+    time.sleep(0.1)
+print()  # quebra linha
+
+for letra3 in texto3:
+    print(letra3, end="", flush=True)
+    time.sleep(0.05)
+print()  # quebra linha
+
+for letra4 in texto4:
+    print(letra4, end="", flush=True)
+    time.sleep(0.09)
+print()  # quebra linha
+
+for letra5 in texto5:
+    print(letra5, end="", flush=True)
+    time.sleep(1)
+print()  # quebra linha
+
+for letra6 in texto6:
+    print(letra6, end="", flush=True)
+    time.sleep(0.09)
+print()  # quebra linha
+
+for letra7 in texto7:
+    print(letra7, end="", flush=True)
+    time.sleep(0.07)
+print()
+
+for letra8 in texto8:
+    print(letra8, end="", flush=True)
+    time.sleep(0.1)
+print()
+
+for letra9 in texto9:
+    print(letra9, end="", flush=True)
+    time.sleep(0.1)
+print()
+
+for letra10 in texto10:
+    print(letra10, end="", flush=True)
+    time.sleep(0.09)
+print()
+
+for letra11 in texto11:
+    print(letra11, end="", flush=True)
+    time.sleep(1)
+print()
+
+for letra12 in texto12:
+    print(letra12, end="", flush=True)
+    time.sleep(0.09)
+print()
+
+for letra13 in texto13:
+    print(letra13, end="", flush=True)
+    time.sleep(0.1)
+print()
+
+for letra14 in texto14:
+    print(letra14, end="", flush=True)
+    time.sleep(0.5)
+print()
+
+for letra15 in texto15:
+    print(letra15, end="", flush=True)
+    time.sleep(0.1)
+print()
+
+for letra16 in texto16:
+    print(letra16, end="", flush=True)
+    time.sleep(0.09)
+print()
+
+for letra17 in texto17:
+    print(letra17, end="", flush=True)
+    time.sleep(0.07)
+print()
 
