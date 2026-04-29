21a5e852d22d35569b851f8b74bd8e63c1958ccf
Geovane <rochageovane05@gmail.com>
2026-04-29 19:13:20 -0300

    Abrir video de fundo

--- code.py
+++ code.py
@@ -1,10 +1,20 @@
 import time
 
+import webbrowser
+
+def abrir_youtube():
+    url = "https://www.youtube.com/watch?v=fK6tf6opIg0&t=63s"
+    webbrowser.open(url)
+
+if __name__ == "__main__":
+    abrir_youtube()
+
+
 texto = "..."
 texto2 = "it's close"
 texto3 = "to midnight"
 texto4 = "and something evil's lurking in the dark"
-texto5 = "..."
+texto5 = "....."
 texto6 = "Under the moonlight"
 texto7 = "You see a sight that almost stops your heart"
 texto8 = "..."
@@ -17,36 +27,43 @@ texto14 = "."
 texto15 = "You're Paralyzed"
 texto16 = "Cause this is thriller"
 texto17 = "Thriller night"
+texto18 = "And no one's gonna save you from the beast about to strike"
+texto19 = "You know it's thrilleeer"
+texto20 = "thriller night"
+texto21 = "You're fighting for your life inside"
+texto22 = "a killer"
+texto23 = "thriller tonight"
+texto24 = "Yeah"
 
 for letra in texto:
     print(letra, end="", flush=True)
-    time.sleep(20) #20
-print()  # quebra linha
+    time.sleep(41) #20
+print()  
 
 for letra2 in texto2:
     print(letra2, end="", flush=True)
     time.sleep(0.1)
-print()  # quebra linha
+print()  
 
 for letra3 in texto3:
     print(letra3, end="", flush=True)
     time.sleep(0.05)
-print()  # quebra linha
+print()
 
 for letra4 in texto4:
     print(letra4, end="", flush=True)
     time.sleep(0.09)
-print()  # quebra linha
+print()  
 
 for letra5 in texto5:
     print(letra5, end="", flush=True)
     time.sleep(1)
-print()  # quebra linha
+print()  
 
 for letra6 in texto6:
     print(letra6, end="", flush=True)
     time.sleep(0.09)
-print()  # quebra linha
+print() 
 
 for letra7 in texto7:
     print(letra7, end="", flush=True)
@@ -103,3 +120,32 @@ for letra17 in texto17:
     time.sleep(0.07)
 print()
 
+for letra18 in texto18:
+    print(letra18, end="", flush=True)
+    time.sleep(0.09)
+print()
+
+for letra19 in texto19:
+    print(letra19, end="", flush=True)
+    time.sleep(0.07)
+print()
+
+for letra20 in texto20:
+    print(letra20, end="", flush=True)
+    time.sleep(0.07)
+print()
+
+for letra21 in texto21:
+    print(letra21, end="", flush=True)
+    time.sleep(0.1)
+print()
+
+for letra22 in texto22:
+    print(letra22, end="", flush=True)
+    time.sleep(0.09)
+print()
+
+for letra23 in texto23:
+    print(letra23, end="", flush=True)
+    time.sleep(0.1)
+print()
